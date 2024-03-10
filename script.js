c82909eed257bb7b79d6362a04efaf05b7e3daf0
Jeremiah Ranen R <141173239+JeremiahRanen7@users.noreply.github.com>
2024-03-10 11:39:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function calculate(expression){
+    try{
+        const result =  new Function('return ' + expression)()
+
+        if(isNaN(result) || !isFinite(result)){
+            throw new Error("Invalid Expression: " + expression)
+        }
+        return result
+    }
+    catch(error){
+        return "Error: " + error.message
+    }
+}
+
+let total = document.getElementById("total")
+let clear = document.querySelector(".clear")
+let remove = document.querySelector(".remove")
+let operator = document.querySelectorAll(".operator")
+let number = document.querySelectorAll(".number")
+let equal = document.querySelector(".equal")
+
+for(let i = 0 ; i<number.length ; i++){
+    number[i].addEventListener("click",function(){
+        total.value += this.value
+    })
+}
+
+for(let i = 0 ; i<operator.length ; i++){
+    operator[i].addEventListener("click",function(){
+        total.value += this.value
+    })
+}
+
+clear.addEventListener("click",function(){
+    total.value = ""
+})
+
+equal.addEventListener("click",function(){
+    total.value = calculate(total.value)
+})
+
+remove.addEventListener("click",function(){
+    total.value = total.value.slice(0,-1)
+})
+
+
+
+
+
